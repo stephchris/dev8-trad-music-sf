51d6d38d222f586e7d9c025023c363fedf394c7a
stephchris <estom35@gmail.com>
2022-12-01 13:46:17 +0100

    README

---
+++ README.md.php
@@ -0,0 +1,13 @@
+# Trad Music (Symfony)
+
+## Étapes de création du projet
+
+Générer le projet Symfony
+
+## Installatiob du projet
+
+Faire un fork du projet puis le cloner (git clone URL)
+
+
+
+
