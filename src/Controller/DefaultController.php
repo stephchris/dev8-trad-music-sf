49ac77ed035ff09e1f9bd4dd40b988595bad5e5a
stephchris <estom35@gmail.com>
2022-12-02 11:51:49 +0100

    Génération des entités gig et pub

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,18 +2,27 @@
 
 namespace App\Controller;
 
+use App\Repository\InstrumentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
-    #[Route('/default', name: 'app_default')]
-    public function index(): JsonResponse
+    #[Route('/', name: 'homepage')]
+    public function index(InstrumentRepository $instrumentRepository): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/DefaultController.php',
+        //Récupérer la liste des instruments en BDD (SELECT*FROM instrument )
+        $instruments = $instrumentRepository->findAll();
+        dump($instruments);
+        // appelle le fichier de template Twig avec la méthode render
+        //permet d'envoyer des données du controller vers la vue homepage
+        // retourner ce qu'i y a dans la page home
+        return $this->render('default/homepage.html.twig', [
+            'instruments' => $instruments
         ]);
+
+
     }
 }
