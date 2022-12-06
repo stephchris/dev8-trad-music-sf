02c9b21a914a9440a49a68e41e45cd940e297abb
stephchris <estom35@gmail.com>
2022-12-06 13:47:43 +0100

    affichage dynamique de la liste des concerts sur la page d'accueil

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Repository\InstrumentRepository;
 use App\Repository\MusicianRepository;
+use App\Repository\GigRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,16 +13,17 @@ use Symfony\Component\Routing\Annotation\Route;
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(MusicianRepository $musicianRepository): Response
+    public function index(MusicianRepository $musicianRepository, GigRepository $gigRepository): Response
     {
         //Récupérer la liste des instruments en BDD (SELECT*FROM instrument )
         $musicians = $musicianRepository->findAll();
-        dump($musicians);
+        $gigs = $gigRepository->findAll();
         // appelle le fichier de template Twig avec la méthode render
         //permet d'envoyer des données du controller vers la vue homepage
         // retourner ce qu'il y a dans la page home
         return $this->render('default/homepage.html.twig', [
-            'musicians' => $musicians
+            'musicians' => $musicians,
+            'gigs' => $gigs
         ]);
 
 
